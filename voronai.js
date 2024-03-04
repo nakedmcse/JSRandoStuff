a9d4d907b0919fc473e3b5391dc312f79935b886
Walker Aldridge <daldridge@lairdscomputer.com>
2024-03-03 23:33:44 -0600

    Voronoi finished

--- voronai.js
+++ voronai.js
@@ -1,4 +1,4 @@
-// Voronai Canvas
+// Voronoi Canvas
 document.body.style.backgroundColor = "#000000";
 const canvas = document.createElement('canvas');
 canvas.id = 'VoronaiCanvas';
@@ -14,40 +14,51 @@ const colors = ['red','green','blue','yellow','pink','brown','black','white'];
 const ctx = canvas.getContext('2d');
 
 // Set start points
-const points = [];
+let points = [];
 for(let i=0; i<8; i++) {
-    points[i] = [Math.floor(Math.random() * canvas.width),Math.floor(Math.random() * canvas.height)];
+    points.push({
+            x: Math.floor(Math.random() * canvas.width),
+            y: Math.floor(Math.random() * canvas.height)
+        }
+    )
 }
 
 // Loop and fill point
 const radius = 4;
-while(true) {
-    let minDist =9999;
-    let minIdx = 8;
-    const x = Math.floor(Math.random() * canvas.width);
-    const y = Math.floor(Math.random() * canvas.height);
+window.requestAnimationFrame(drawVeronoi);
 
-    // Mutate points
-    for(let i = 0; i<8; i++) {
-        // Mutate points here
-        // Get distance
-        // Check for min and set minidx
+function drawVeronoi() {
+    for(let l=0; l<100; l++) {
+        let minDist = 9999;
+        let minIdx = 8;
+        const x = Math.floor(Math.random() * canvas.width);
+        const y = Math.floor(Math.random() * canvas.height);
+
+        // Mutate points
+        for (let i = 0; i < 8; i++) {
+            // Mutate points here
+            points[i].x = points[i].x + (Math.random() - 0.5 > 0 ? 1 : -1);
+            points[i].y = points[i].y + (Math.random() - 0.5 > 0 ? 1 : -1);
+            if (points[i].x < 0) points[i].x = 0;
+            if (points[i].y < 0) points[i].y = 0;
+            if (points[i].x > canvas.width) points[i].x = canvas.width;
+            if (points[i].y > canvas.height) points[i].y = canvas.height;
+            // Get distance - manhattan formula
+            const d = Math.abs(x - points[i].x) + Math.abs(y - points[i].y);
+            // Check for min and set minidx
+            if (d < minDist) {
+                minDist = d;
+                minIdx = i;
+            }
+        }
+
+        // Draw circle of minIdx color at x,y
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.closePath();
+        ctx.fillStyle = colors[minIdx];
+        ctx.fill();
     }
 
-    // Draw circle of minIdx color at x,y
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = colors[minIdx];
-    ctx.fill();
+    window.requestAnimationFrame(drawVeronoi);
 }
-
-// Get the drawing context of the canvas
-/*
-ctx.beginPath();
-ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.lineWidth = 5;
-ctx.strokeStyle = '#003300';
-ctx.stroke();
-*/
