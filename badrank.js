957495fc1c85777d49331be649377fd0eb4f288e
Walker Aldridge <daldridge@lairdscomputer.com>
2024-07-27 21:08:57 -0500

    MOre cursed stuff

--- badrank.js
+++ badrank.js
@@ -3,4 +3,9 @@ function rank(score) {
     return score > 90 ? "A" : score > 80 ? "B" : score > 70 ? "C" : score > 60 ? "D" : "E"
 }
 
+function cursedRank(score) {
+    return "EEEEEEDCBA".charAt(Math.floor(score/10))
+}
+
 console.log(rank(95), rank(75), rank(65), rank (40));
+console.log(cursedRank(95), cursedRank(75), cursedRank(65), cursedRank (40));
---
+++ nosemis.js
@@ -0,0 +1,4 @@
+// Javascript with no semi colons
+{ console.log("\nLook Ma") }
+{ console.log("JavaScript with no semi colons") }
+{ console.log("WTAF") }
