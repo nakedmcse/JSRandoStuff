44f0826de9867a2ceb762a1b355387e7fd282cc8
Walker Aldridge <daldridge@lairdscomputer.com>
2024-11-01 16:45:42 -0500

    Strip chars stringbuilder with tests

--- stripchars.js
+++ stripchars.js
@@ -1,4 +1,6 @@
 // Strip any repeating chars beyond the given number
+const fs = require("fs");
+
 function generateLongCode() {
     const outputSB = [];
     const choices = ['a','b','c','d'];
@@ -45,12 +47,37 @@ function stripcharsRegex(s, limit) {
 console.log(`Example 1: should return leetcode: ${stripcharsSB("leeetcode", 3)}`);
 console.log(`Example 2: should return aabaa: ${stripcharsSB("aaabaaaa", 3)}`);
 console.log(`Example 3: should return aab: ${stripcharsSB("aab", 3)}`);
+console.log();
+
+const test = fs.readFileSync('long_string.txt').toString();
+const starttestR = performance.now();
+const rettestR = stripcharsRegex(test, 3);
+const endtestR = performance.now();
+console.log(`Test regex pattern completed in ${endtestR - starttestR} ms`);
+
+const starttestSB = performance.now();
+const rettestSB = stripcharsSB(test, 3);
+const endtestSB = performance.now();
+console.log(`Test stringbuilder pattern completed in ${endtestSB - starttestSB} ms`);
+
+const starttestS = performance.now();
+const rettestS = stripchars(test, 3);
+const endtestS = performance.now();
+console.log(`Test strings pattern completed in ${endtestS - starttestS} ms`);
+console.log();
+
 const longtest = generateLongCode();
 const startSB = performance.now();
 const retSB = stripcharsSB(longtest, 3);
 const endSB = performance.now()
 console.log(`Stringbuilder pattern completed in ${endSB - startSB} ms`);
+
 const startStr = performance.now();
 const retStr = stripchars(longtest, 3);
 const endStr = performance.now()
 console.log(`String pattern completed in ${endStr - startStr} ms`);
+
+const startR = performance.now();
+const retR = stripcharsRegex(longtest, 3);
+const endR = performance.now();
+console.log(`Regex pattern completed in ${endR - startR} ms`);
