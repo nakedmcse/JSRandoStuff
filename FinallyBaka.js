0cebb571cc4d21196050176db48b43316c1a566c
Walker Aldridge <daldridge@lairdscomputer.com>
2025-03-08 12:00:36 -0600

    try finally returns

---
+++ FinallyBaka.js
@@ -0,0 +1,10 @@
+function finallyBaka() {
+    try {
+        return "what you would expect";
+    }
+    finally {
+        return "go home javascript you're drunk";
+    }
+}
+
+console.log(finallyBaka());
