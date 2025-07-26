08495a0239e7415e9d19dffd01e3ba1d7b7a4e21
Walker Aldridge <daldridge@lairdscomputer.com>
2025-07-26 10:55:11 -0500

    UK IP detection middleware for express

--- apiEcho.mjs
+++ apiEcho.mjs
@@ -1,6 +1,20 @@
 import express from 'express'
+import maxmind from 'maxmind'
+
 const echoApp = express();
 echoApp.use(express.json());
+
+const detectUK = async function (req, res, next) {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const lookup = await maxmind.open('Geolite2-Country.mmdb');
+    const iplookup = lookup.get(ip);
+    if(iplookup.country.iso_code === 'GB') {
+        // GB IP detected - do something
+        console.log("UK IP DETECTED");
+    }
+    next();
+}
+echoApp.use(detectUK);
 
 echoApp.get('/', (req, res) => {
     console.log(req.query);
